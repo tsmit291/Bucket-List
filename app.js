e5f0e3ca23606de3745466c6ca6e663db88c3f95
= <tsmit291@icloud.com>
2016-02-01 10:28:54 -0700

    added admin and auth requires and app.use in app.js file

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var admin = require('./routes/admin');
+var auth = require('./routes/auth');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/admin', admin);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
