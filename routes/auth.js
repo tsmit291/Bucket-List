cbef60dc8af1499ee28dac123dcda5226c5fbc26
= <tsmit291@icloud.com>
2016-02-01 11:10:40 -0700

    added facebook callback routes in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/auth/facebook', function(req, res, next));
+
+router.get('/auth/facebook/callback', function(req, res, next));
+
 
 
 
