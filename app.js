5e94b1039a89c7f6b78f1de5f32b7754006b8306
Scott Christensen <scottmc526@gmail.com>
2016-02-01 13:23:47 -0700

    facebook auth success

--- app.js
+++ app.js
@@ -23,9 +23,13 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(cb);
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
   }
 ));
 
