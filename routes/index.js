98ff71741d9c1786dd156b3cdcc1706a6d42bddc
= <tsmit291@icloud.com>
2016-02-01 10:24:45 -0700

    index.js routes in the works

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET community index page */
+router.get('/bucketlists', function(req, res, next){
+  res.render('index');
+});
+
+/* GET personal bucket list page */
+router.get('/bucketlists/:id', function(req, res, next){
+  
+})
+
 module.exports = router;
