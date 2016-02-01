8033deddd641d8c37a81861949bdff97b8a3c86a
= <tsmit291@icloud.com>
2016-02-01 14:15:11 -0700

    syntactical error fixed

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,11 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/bucketlists'
+    connection: 'postgres://localhost/bucketlist'
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true'
   }
+};
