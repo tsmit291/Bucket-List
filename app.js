488d128f912836d4b88cf9564126e3cbdd4d8110
= <tsmit291@icloud.com>
2016-02-02 16:15:18 -0700

    correctly logs user in and redirects to community index page

--- app.js
+++ app.js
@@ -1,36 +1,67 @@
 require('dotenv').config()
 var express = require('express');
+var knex = require('knex');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 var app = express();
+var session = require('cookie-session');
 var passport = require('passport')
 var FacebookStrategy = require('passport-facebook').Strategy;
+
+function User (){
+  return knex({
+    client: 'pg',
+    connection: 'postgres://localhost/bucketlist'
+  })('users');
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: process.env.HOST+'/auth/facebook/callback'
+    // don't forget to config your heroku HOST environment variable
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(accessToken);
-    console.log(refreshToken);
+  function(accessToken, refreshToken, profile, done) {
+    console.log("***PROFILE**");
     console.log(profile);
-    console.log(cb);
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    process.nextTick(function () {
+      User().where('fb_id', profile.id).first().then(function (user) {
+        if(user){
+          req.session.user = profile.id;
+        // set user session
+        } else {
+          User().insert({fb_id: profile.id});
+          console.log(fb_id);
+          // insert into database
+        }
+      })
+    })
+    done(null, profile);
   }
 ));
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(null, user)
+  });
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,25 @@ var express = require('express');
 var router = express.Router();
 var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
+var knex = require('../db/knex');
 
+function User() {
+  return knex('users')
+};
+
+/*login*/
 router.get('/facebook', passport.authenticate('facebook'));
 
-router.get('/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/bucketlists');
-  });
+/*logout*/
+router.get('/logout', function(req, res, next){
+  req.session = null;
+  res.redirect('/');
+});
 
-
-
-
-
-
+/* route that facebook will call once user has authenticated properly*/
+router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
+function (req, res, next) {
+  res.redirect('/bucketlists');
+});
 
 module.exports = router;
