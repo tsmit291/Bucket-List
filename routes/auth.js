df98749c6ff5c686235fa9c4360debff3c03ffb3
= <tsmit291@icloud.com>
2016-02-04 14:55:23 -0700

    login branch merged with master

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
 var knex = require('../db/knex');
+var bcrypt = require('bcrypt');
 
 function User() {
   return knex('users')
 };
 
-/*login*/
+/*login with facebook*/
 router.get('/facebook', passport.authenticate('facebook'));
 
 /*logout*/
@@ -22,5 +23,32 @@ router.get('/facebook/callback', passport.authenticate('facebook', { failureRedi
 function (req, res, next) {
   res.redirect('/bucketlists');
 });
+
+/* log in without facebook */
+router.post('/login', function(req, res, next){
+  User().where({email:req.body.email}).first().then(function(found){
+    if (found){
+      if (bcrypt.compareSync(req.body.password, found.password)){
+        res.cookie('user', req.body.email)
+        res.redirect('/bucketlists');
+      } else {
+        res.send('Email/Password not valid')
+      }
+    } else {
+      res.send('Email/Password not valid')
+    }
+  })
+});
+
+/* sign up */
+router.post('/signup', function(req, res, next){
+  var crypted = bcrypt.hashSync(req.body.password, 8)
+  User().insert({email: req.body.email, password: crypted, display_name: req.body.display_name}).then(function(result){
+    res.cookie('user', req.body.email)
+    res.redirect('/bucketlists');
+  });
+});
+
+
 
 module.exports = router;
