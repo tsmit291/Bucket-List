09b8bc22e6c14143468a2afe88d04a42a70a9c02
Aubrey Ford <musicaubrey@gmail.com>
2016-02-04 10:55:27 -0700

    mvp functionality complete

--- routes/index.js
+++ routes/index.js
@@ -21,28 +21,50 @@ router.get('/bucketlists', function(req,res){
   if (querystring) {
     search=querystring
   }else{
-    search='Aubrey'
+    search='Hang Gliding'
   }
 
-  knex.raw("select users.id as users_id, bucketlist.id as bucketlist_id, users.picture as users_picture, bucketlist.picture as bucketlist_picture, bucketlist.title as bucketlist_title from users inner join bucketlist on users.id = bucketlist.user_id where display_name like  '%"+search+"%'").then(function(bucketlists){
-    var bucketlists = bucketlists.rows
-    var idsArray = []
-    bucketlists.forEach(function(e){
-      idsArray.push(e['users_id'])
+  // knex.raw("select users.id as users_id, bucketlist.id as bucketlist_id, users.picture as users_picture, bucketlist.picture as bucketlist_picture, bucketlist.title as bucketlist_title from users inner join bucketlist on users.id = bucketlist.user_id where display_name like  '%"+search+"%'").then(function(bucketlists){
+  //   var bucketlists = bucketlists.rows
+  //   var idsArray = []
+  //   bucketlists.forEach(function(e){
+  //     idsArray.push(e['users_id'])
+  //   })
+  //   Users().whereIn('id', idsArray).then(function(users){
+  //     console.log(users)
+  //   })
+
+
+  knex.raw("select user_id, id from bucketlist where title like '%"+search+"%'").then(function(searchUsers){
+    console.log('SEARCHUSERS')
+    // console.log(searchUsers.rows)
+    var users = []
+    var boldItem = []
+      searchUsers.rows.forEach(function(e){
+        users.push(e['user_id'])
+        boldItem.push(e['id'])
+      })
+      users = users.filter(function(curr,idx,arr){
+        return arr.indexOf(curr) === idx
+      })
+
+        console.log('USERS')
+        // console.log(users)
+        console.log('BOLDITEM')
+        // console.log(boldItem)
+        Users().whereIn('id', users).then(function(userData){
+        console.log('USERDATA')
+        // console.log(userData)
+          Bucketlist().whereIn('user_id', users).then(function(bucketlists){
+            console.log('BUCKETLISTS')
+            // console.log(bucketlists)
+            console.log(bucketlists)
+            // console.log(userData)
+            // console.log(boldItem)
+            res.render('index', {bucketlists:bucketlists, userData:userData, boldItem:boldItem});
+          })
+        })
     })
-    Users().whereIn('id', idsArray).then(function(users){
-      console.log(users)
-    })
-
-
-  knex.raw("select user_id from bucketlist where title like '%"+search+"%'").then(function(searchUser){
-    console.log(searchUser)
-  })
-
-
-
-  res.render('index', {bucketlists:bucketlists});
-  })
 })
 
 //Upon SEARCH
