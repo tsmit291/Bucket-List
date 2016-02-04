63742e802af0eef02a1266754a3245dbf76d5e81
= <tsmit291@icloud.com>
2016-02-04 13:55:35 -0700

    password column added

--- migrations/20160204134352_create_login.js
+++ migrations/20160204134352_create_login.js
@@ -1,18 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function(table){
-    table.increments();
-    table.string('fb_id');
-    table.string('display_name');
-    table.boolean('auth');
-    table.string('email');
+  return knex.schema.table('users', function(table){
     table.string('password');
-    table.text('bio');
-    table.string('picture');
-    table.json('bucketlists');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return knex.schema.table('users', function(table){
+    table.dropColumn('password')
+  });
 };
