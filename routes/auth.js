b08714ec1b99768349985ac29625b019896b9003
= <tsmit291@icloud.com>
2016-02-04 14:13:59 -0700

    bcrypt works

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
 var knex = require('../db/knex');
+var bcrypt = require('bcrypt');
 
 function User() {
   return knex('users')
@@ -21,6 +22,15 @@ router.get('/logout', function(req, res, next){
 router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
 function (req, res, next) {
   res.redirect('/bucketlists');
+});
+
+/* sign up */
+router.post('/signup', function(req, res, next){
+  var crypted = bcrypt.hashSync(req.body.password, 8)
+  User().insert({email: req.body.email, password: crypted}).then(function(result){
+    res.cookie('user', req.body.email)
+    res.redirect('/bucketlists');
+  });
 });
 
 module.exports = router;
