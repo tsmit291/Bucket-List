30f47d10cfde6e5092ae3b8f82204ed4c23761dc
Aubrey Ford <musicaubrey@gmail.com>
2016-02-04 23:12:50 -0700

    aaa

--- public/javascripts/autoqueries.js
+++ public/javascripts/autoqueries.js
@@ -1,1 +1,1 @@
-module.exports=['Hang gliding', 'Skydiving', 'Marathon', 'Fishing', 'Rock', 'Backpack', 'Climb', 'Eat', 'Leprechaun', 'Dog show', 'Fly', 'Ghost hunting', 'Visit Petra', 'Water ski', 'Jet ski']
+module.exports=['Hang gliding', 'Skydiving', 'Marathon', 'Fishing', 'Rock', 'Backpack', 'Climb', 'Eat a sea urchin', 'Catch a leprechaun', 'Dog show', 'Fly', 'Ghost hunting', 'Visit Petra', 'Water ski', 'Jet ski']
